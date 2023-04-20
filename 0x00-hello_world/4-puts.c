99b976077ea45c93dc8df9a653374962309d5a4f
YasmineAche <achemine.yasmine@gmail.com>
2023-04-20 16:56:02 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Prints a sentence to the console
+ *
+ * Description: A longer description of the function and what it does
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
