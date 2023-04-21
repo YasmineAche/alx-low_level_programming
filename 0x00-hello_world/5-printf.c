bc5cd576479f279271be6f03fa58478d43a854ad
YasmineAche <achemine.yasmine@gmail.com>
2023-04-20 17:02:19 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a specific message
+ * Return: 0 on success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
