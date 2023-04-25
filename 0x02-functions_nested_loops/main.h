fae77f4603e5bc6670b5f699998218af9a35ef15
YasmineAche <achemine.yasmine@gmail.com>
2023-04-25 14:13:18 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,5 @@
   6  * Return: On success 1.
   7  * On error, -1 is returned, and errno is set appropriately.
   8  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet(void);
