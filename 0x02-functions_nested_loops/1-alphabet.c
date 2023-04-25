a3afb51ffb150a94242d8a917a8661fd154ea570
YasmineAche <achemine.yasmine@gmail.com>
2023-04-25 15:10:12 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 /**
   * print_alphabet - writes the alphabet
@@ -7,21 +6,11 @@
   */
 void print_alphabet(void)
 {
-	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+//	char letters[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	for (i = 0; i < 26; i++)
 	{
 		_putchar(letters[i]);
 	}
 	_putchar('\n');
-}
-/**
- * main - Entry point
- *
- * Return: Always 0 (Error)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
