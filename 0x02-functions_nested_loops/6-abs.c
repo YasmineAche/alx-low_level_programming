7460310d58da6460677e0f61c60578d276427772
YasmineAche <achemine.yasmine@gmail.com>
2023-04-26 11:41:41 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * _abs - writes the absulut value of a number
+  *
+  * Return: abs(n)
+  */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else
+	{
+		return (n);
+	}
+}
