ef7d0cc36b270d51126f0bb36a3b840e96b915ce
YasmineAche <achemine.yasmine@gmail.com>
2023-05-02 22:20:10 +0100

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * puts2 - prints every other character of a string
+  *
+  * @str : pointer to the variable
+  *
+  * Return : always 0
+  */
+void puts2(char *str)
+{
+	int len = 0, i;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str -= len;
+	for (i = 0; i < len / 2; i++)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
