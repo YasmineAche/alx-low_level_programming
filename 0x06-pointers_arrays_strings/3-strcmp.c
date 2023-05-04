ff1bb51686b633ffcdf8c27482f4cdd48eb20689
YasmineAche <achemine.yasmine@gmail.com>
2023-05-04 09:08:21 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,31 +9,15 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1 = 0, i = 0, len_s2 = 0;
+	int i = 0;
 
-	while (*s1 != '\0')
+	while (*s1 != '\0' || *s2 != '\0')
 	{
-		len_s1++;
-		s1++;
-	}
-	while (*s2 != '\0')
-	{
-		len_s2++;
-		s2++;
-	}
-	s1 -= len_s1;
-	s2 -= len_s2;
-	while (s1[i] != '\0' || s2[i] != '\0')
-	{
-		if (s1[i] != s2[i] && len_s1 > len_s2)
+		if (s1[i] != s2[i])
 		{
-			return (15);
+			return (s1[i] - s2[i]);
 		}
-		else if (s1[i] != s2[i] && len_s1 < len_s2)
-		{
-			return (-15);
-		}
-		else if (s1[i] == s2[i] && *s1 == *s2)
+		else if (s1[i] == '\0' && s2[i] == '\0')
 		{
 			break;
 		}
