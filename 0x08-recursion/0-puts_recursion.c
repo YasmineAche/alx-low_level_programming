011e50965e61261efaf65929103f2121588a9f8e
YasmineAche <achemine.yasmine@gmail.com>
2023-05-10 22:22:11 +0100

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - faction
+ *
+ * @s : pointer
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\0');
+		return;
+	}
+}
