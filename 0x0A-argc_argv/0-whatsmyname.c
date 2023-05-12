d8461d6d50c1dac2ce43d50f73fe4eb72d686f94
YasmineAche <achemine.yasmine@gmail.com>
2023-05-12 19:29:11 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main intry point
+ *
+ * @argc : int variable
+ * @argv : pointer to list of strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
