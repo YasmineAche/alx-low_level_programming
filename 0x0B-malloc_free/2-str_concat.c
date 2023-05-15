1fccc59a5b64f9545a01cf85df3fb689602a9109
YasmineAche <achemine.yasmine@gmail.com>
2023-05-15 08:26:15 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * str_concat - fonction
+  *
+  * @s1 : variable
+  * @s2 : variable
+  *
+  * Return: Null if alloc's size is 0 or pointer
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int i, len1 = 0, len2 = 0;
+
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len1; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (i = 0; i < len2; i++)
+		{
+			p[i + len1] = s2[i];
+		}
+		p[len1 + len2] = '\0';
+		return (p);
+	}
+}
