ac522648e6c1358b814b74d0712b4d48a66c875e
YasmineAche <achemine.yasmine@gmail.com>
2023-05-18 14:06:06 +0100

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,11 +22,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	while (s1[len1] == '\0')
+	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-	while (s2[len2] == '\0')
+	while (s1 && s2[len2])
 	{
 		len2++;
 	}
