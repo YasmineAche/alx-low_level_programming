2c1b2dc1f7a70c14f52811f527f67b3d810300bc
YasmineAche <achemine.yasmine@gmail.com>
2023-05-18 14:49:59 +0100

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _calloc - fonction
+  *
+  * @nmemb : variable
+  * @size : variable
+  *
+  * Return: nothing
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *p, i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(size) * nmemb);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
