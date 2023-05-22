7b0d35ce8eaccbafba08a79d82b9e7bf3bbee7bc
YasmineAche <achemine.yasmine@gmail.com>
2023-05-22 09:35:23 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+  * struct dog - structure
+  * @name: variable
+  * @age: variable
+  * @owner: variable
+  *
+  * Description : long description
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
