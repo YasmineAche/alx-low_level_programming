01b868487361ce4ccab7bd7700b553103b4463df
YasmineAche <achemine.yasmine@gmail.com>
2023-05-23 03:11:46 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
   * struct dog - structure
   * @name: variable
@@ -11,4 +13,16 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+/**
+  * prototypes
+  */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
