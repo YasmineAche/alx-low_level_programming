db12a67997893823153645e3e545dfc2f30ee103
YasmineAche <achemine.yasmine@gmail.com>
2023-06-05 14:07:17 +0100

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * print_listint_safe - fonction
+  *
+  * @head: pointer
+  *
+  * Return: size_t
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *current = head;
+
+	while (current)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		if (current->next >= current)
+		{
+			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			break;
+		}
+		current = current->next;
+	}
+	return (count);
+}
