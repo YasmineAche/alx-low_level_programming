65cee5b8d3b4647077ea97453384be51c2a05e79
YasmineAche <achemine.yasmine@gmail.com>
2023-06-05 12:38:15 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * free_listint2 - fonction
+  *
+  * @head: pointer to pointer
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+}
