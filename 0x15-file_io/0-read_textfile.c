424c937330250812ce2581c06578e0352680aed4
YasmineAche <achemine.yasmine@gmail.com>
2023-06-12 21:26:00 +0100

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,23 @@
 #include "main.h"
 /**
   * read_textfile- fonction
+  *
   * @filename: pointer
   * @letters: argument
+  *
   * Return: ssize_t
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytes;
-	int fd = open(filename, O_RDONLY);
+	int fd;
 	char buffer[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 	{
 		return (0);
 	}
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
