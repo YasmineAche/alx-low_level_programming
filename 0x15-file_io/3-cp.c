ee9fae62165d8ffd4b3b7b1b1759922b62170281
YasmineAche <achemine.yasmine@gmail.com>
2023-06-12 22:38:05 +0100

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, \
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -34,9 +35,7 @@ int main(int argc, char *argv[])
 		}
 		num2 = write(file_to, buf, num1);
 		if (num2 < num1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-		}
 	}
 	if (close(file_from) == -1)
 	{
